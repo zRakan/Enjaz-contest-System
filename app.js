9c0ad40effb61a514da8860c287ddb7f25f0ecf2
zRakan <irakanalmutairi@gmail.com>
2023-09-13 20:34:09 +0300

    Initial commit for branch

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 import express from "express";
-import wSocket from "express-ws";
+import { Server } from "socket.io";
+
 import fs from "fs";
 
 
-const app = express(); // Creating App & Websocket
-const websocket = wSocket(app);
+const app = express(); // Creating App
 const PORT = 3000;
-
-// Reading JSON file
-let studentsData = JSON.parse(fs.readFileSync('./students.json'));
-console.log(studentsData);
-
-let winners = [];
-
-for(let student in studentsData) {
-    if(studentsData[student].winner)
-        winners.push({
-            contestant: studentsData[student].name,
-            winAt: studentsData[student].finished
-        });
-}
-
-console.log(winners);
-
 
 // Initialize middle-ware(s)
 app.use(express.json()); // Bodyparser built-in function
@@ -34,97 +17,33 @@ app.set('view engine', 'ejs');
 // Static files
 app.use(express.static('views/static'))
 
+// Matchmaking information
+let playersConnected = 0;
+
 app.get('/', function(req, res) {
-    res.render("index", { data: "Test2" });
+    res.render("index", { data: playersConnected });
 });
 
 
-// Websocket
-app.ws('/controller', function(ws, req) {
-
-    // Receiver
-    ws.on('message', function(message) {
-        console.log(`Message from client: ${message}`)
-    });
-
-    // Sender
-    ws.send(JSON.stringify({ type: "echo" }));
-});
-
-// Broadcast websocket
-const websocketClients = websocket.getWss('/controller');
-
-let stoppedTimer, currentTimer, currentContestant;
-app.post('/contestant', function(req, res) {
-    const contestant = req.body["name"]; // Contestant name
-    const phoneNumber = req.body["pNumber"]; // Phone number
-
-    // Reset stopped timer
-    stoppedTimer = null;
-
-    // Current state
-    currentContestant = phoneNumber; // contestant
-    currentTimer = new Date(); // Counting date
-
-    studentsData[phoneNumber] = {
-        name: contestant
-    };
-
-    // Save student data
-    fs.writeFileSync("./students.json", JSON.stringify(studentsData, null, 4));
-
-
-    res.send({ status: "success" });
-
-    // Send to all channels (clients)
-    websocketClients.clients.forEach(function(client) {
-        client.send(JSON.stringify({ type: "starting", contestant: contestant }));
-    });
-});
-
-app.post('/stop', function(req, res) {
-    console.log("Stopped");
-    stoppedTimer = new Date();
-
-    // Send to all channels (clients)
-    websocketClients.clients.forEach(function(client) {
-        client.send(JSON.stringify({ type: "stop" }));
-    });
-    
-    res.send({ status: "success" });
-});
-
-
-app.post('/judged', function(req, res) {
-    const judgeResult = req.body["result"]; // [correct, wrong];
-    const isWinner = judgeResult == 'correct';
-    const finishedAt = (stoppedTimer ? stoppedTimer : new Date()) - currentTimer;
-
-    if(isWinner) {
-        studentsData[currentContestant].winner = true;
-        studentsData[currentContestant].finished = finishedAt;    
-
-        winners.push({
-            contestant: studentsData[currentContestant].name,
-            winAt: finishedAt
-        });
-
-        // Save student data
-        fs.writeFileSync("./students.json", JSON.stringify(studentsData, null, 4));
-    }
-
-    // Send to all channels (clients)
-    websocketClients.clients.forEach(function(client) {
-        client.send(JSON.stringify({ type: "finished", winner: isWinner, diff: finishedAt }));
-    });
-
-    res.send({ status: "success" });
-});
-
-app.get("/winners", function(req, res) {
-    res.send({ status: "success", data: winners })
-})
-
-app.listen(PORT, function() {
+let serverListener;
+serverListener = app.listen(PORT, function() {
     console.log("Webserver Started");
-})
+});
+
+// Creating websocket
+let websocket = new Server(serverListener);
+
+websocket.on('connection', function(ws) {
+    console.log('a user connected');
+
+    ws.emit('enjaz:updating', { connectedUsers: playersConnected });
+
+    ws.on('disconnect', function() {
+        console.log("User disconnected");
+    });
+
+    ws.on('enjaz:new-contestant', function() {
+        // Websocket broadcast
+        websocket.emit('enjaz:updating', { connectedUsers: ++playersConnected });
+    });
+});
--- views/static/index.js
+++ views/static/index.js
@@ -1,138 +1,20 @@
-/*
-    Helper function
-*/
+document.addEventListener("DOMContentLoaded", function() {
+    const socket = io();
 
-function formatTime(date) {
-    let minutes = Math.floor((date % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((date % (1000 * 60)) / 1000);
-    
-    return [minutes, seconds];
-}
+    const connectedButton = document.querySelector('#contestant-submit');
+    const connectedUsers = document.querySelector('.contestant-container > p');
 
-const usersList = document.querySelector("#top-users");
-function addUserToLeaderboard(data) {
-    const [minutes, seconds] = formatTime(data.winAt);
-    const formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;;
+    let isConnected = false;
+    connectedButton.addEventListener('click', function() {
+        if(isConnected) return;
 
-    usersList.innerHTML += `<div id="user">
-        <p>#${data.pos} ${data.contestant}</p>
-        <p>(<span id="user-time">${formattedTime}</span>)</p>
-    </div>`
-}
+        socket.emit("enjaz:new-contestant")
 
-
-addEventListener("load", async function() {
-    // Variables
-    let winners = []; // List of winners
-    let resp = await fetch("http://localhost:3000/winners");
-        resp = await resp.json();
-        winners = resp.data;
-
-
-    // Sort winners
-    winners.sort(function(a, b) { return a.winAt - b.winAt });
-
-    for(let i in winners) {
-        if(i > 4) break;
-
-        addUserToLeaderboard({
-            pos: parseInt(i)+1,
-            contestant: winners[i].contestant,
-            winAt: winners[i].winAt
-        });
-    }
-
-    // Create WebSocket connection.
-    let socket = new WebSocket("ws://localhost:3000/controller");
-
-    // Connection opened
-    socket.addEventListener("open", function(event) {
-        console.log("Connected");
+        isConnected = true;
     });
 
-    // Listen for messages
-    let currentInterval;
-
-    const contestant = document.querySelector("#contestant");
-    const timer = document.querySelector("#timer");
-    socket.addEventListener("message", function(event) {
-        const data = JSON.parse(event.data);
+    socket.on('enjaz:updating', function(data) {
         console.log(data);
-
-        switch(data.type) {
-            case "starting": // Starting
-                if(currentInterval) clearInterval(currentInterval);
-
-                const currentTime = new Date();
-                contestant.innerHTML = data.contestant; // Set name
-
-                currentInterval = setInterval(async function() {
-                    console.log("Counting...");
-                    const [minutes, seconds] = formatTime(new Date() - currentTime);
-                    if(minutes == 2) {
-                        console.log("Time-up");
-                        timer.innerHTML = 'انتهى الوقت !';
-
-                        await fetch("http://localhost:3000/stop", { method: "POST" });
-
-                        setTimeout(function() {
-                            timer.innerHTML = 'في انتظار النتائج...'
-                        }, 1500);
-
-                        clearInterval(currentInterval);
-                        return;
-                    }
-
-                    timer.innerHTML = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-                }, 500); // 500ms update
-
-                break;
-
-            case "stop":
-                clearInterval(currentInterval); // Clear interval
-                timer.innerHTML = 'في انتظار النتائج...'
-                break;
-            case "finished": // Finished [Winner, loser]
-                clearInterval(currentInterval); // Clear interval
-
-                timer.style.color = data.winner ? "green" : "red";
-                timer.innerHTML = data.winner ? "إجابة صحيحة" : "إجابة خاطئة";
-
-                data.winner && winners.push({
-                    contestant: contestant.innerHTML,
-                    winAt: data.diff
-                });
-
-                // Leaderboard update
-                // Sort winners
-                winners.sort(function(a, b) { return a.winAt - b.winAt });
-                usersList.innerHTML = ""; // Reset leaderboard
-
-                for(let i in winners) {
-                    if(i > 4) break;
-                    
-                    addUserToLeaderboard({
-                        pos: parseInt(i)+1,
-                        contestant: winners[i].contestant,
-                        winAt: winners[i].winAt
-                    });
-                }
-
-
-                // Reset Text
-                setTimeout(function() {
-                    timer.style.color = "#144f43";
-                    contestant.innerHTML = 'في انتظار منافس...';
-                    timer.innerHTML = '00:00';
-                }, 3000);
-                
-
-                break;
-        }
-
-    });
-
-    socket.addEventListener("close", function(event) {
-        console.log(event);
+        connectedUsers.innerHTML = `عدد المتصلين: ${data.connectedUsers}`;
     });
 });
